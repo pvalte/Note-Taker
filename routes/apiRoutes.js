e195338fb4eee2ddff774d30da1523f8cb937afc
Paige A <pvaltemare@gmail.com>
2021-11-02 15:20:06 -0700

    add data validation

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,4 +1,4 @@
-const { createNewNote, deleteNote } = require('../lib/notes.js');
+const { createNewNote, validateNote, deleteNote } = require('../lib/notes.js');
 const { notes } = require('../db/db.json');
 const router = require('express').Router();
 const { v4: uuidv4 } = require('uuid');
@@ -11,13 +11,17 @@ router.get('/notes', (req, res) => {
 });
 
 //post new notes
-//TODO: add validation
 router.post('/notes', (req, res) => {
     // set id based on what the next index of the array will be
     req.body.id = uuidv4();
-    console.log(req.body);
-    const note = createNewNote(req.body, notes);
-    res.json(note);
+
+    // if any data in req.body is incorrect, send 400 error back
+    if (!validateNote(req.body)) {
+        res.status(400).send('The note is missing a title and/or body text.');
+    } else {
+        const note = createNewNote(req.body, notes);
+        res.json(note);
+    }
 });
 
 module.exports  = router;
